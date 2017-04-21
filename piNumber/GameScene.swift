8cee17bff5fdb80c45633d15ba076e37d3260814
Mashael <misha@Mishas-Mac.local>
2017-04-21 06:11:56 +0300

    Initial Commit

---
+++ piNumber/GameScene.swift
@@ -0,0 +1,62 @@
+//
+//  GameScene.swift
+//  piNumber
+//
+//  Created by Misha on 4/18/17.
+//  Copyright © 2017 SMORE. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    private var line = pi()
+    
+    override func didMove(to view: SKView) {
+        
+        line.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2)
+        self.addChild(line)
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ piNumber/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  piNumber
+//
+//  Created by Misha on 4/18/17.
+//  Copyright © 2017 SMORE. All rights reserved.
+//
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from ‘GameScene.sks’
+            let scene = GameScene()
+            
+            
+            scene.size = view.bounds.size
+            scene.scaleMode = SKSceneScaleMode.resizeFill
+            
+            view.presentScene(scene)
+            //view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren’t in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
